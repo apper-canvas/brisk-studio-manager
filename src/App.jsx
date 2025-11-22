653849aa7e2a809cd034b121785b0f7dfd27ea4f
Vivek-Gotraj <123731082+Vivek-Gotraj@users.noreply.github.com>
2025-11-22 16:35:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
+  const name = data.name;
   return (
     <>
       <ToastContainer 
